c99d0cb5b407f8ab93a4a12be9addc3b3bfdcbb5
Brooks Van Buren <wvanbure@purdue.edu>
2017-10-01 14:56:43 -0400

    Testing qr generation

--- src/AppBundle/Controller/QRController.php
+++ src/AppBundle/Controller/QRController.php
@@ -11,18 +11,30 @@ use Endroid\QrCode\QrCode;
 class QRController extends Controller
 {
     /**
-     * @Route("/generateqr", name="homepage")
+     * @Route("/generateqr")
      */
     public function indexAction(Request $request)
     {
         // Get parameter sent with request
 
         // Get QR Code from handler function using parameter
+        $qr = $this->generateQR('test');
 
         // Return the QR code (as an image maybe?)
+        header('Content-Type: ' . $qr->getContentType());
+        return $qr->;
+
+//        $response = new Response($qr->writeString(), Response::HTTP_OK, ['Content-Type' => $qr->getContentType()]);
     }
 
     public function generateQR($url) {
+        // TODO: This should come somewhere in the config
+        $base_url = "localhost:8080/";
+        $full_url = $base_url . $url;
 
+        $qrCode = new QrCode($full_url);
+        $qrCode->setSize(300);
+
+        return $qrCode;
     }
 }
