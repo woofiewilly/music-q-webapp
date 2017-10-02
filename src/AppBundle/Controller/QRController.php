814e6ade261d3a2964bfd07e08e2e15a15308423
Brooks Van Buren <wvanbure@purdue.edu>
2017-10-02 13:19:53 -0400

    Cleaned up QR handler

--- src/AppBundle/Controller/QRController.php
+++ src/AppBundle/Controller/QRController.php
@@ -11,11 +11,13 @@ use Endroid\QrCode\QrCode;
 class QRController extends Controller
 {
     /**
+     * GET this URL to receive a PNG QR code. (boy that's a lot of caps)
+     *
      * @Route("/generateqr/{room_code}")
      */
-    public function indexAction(Request $request, $room_code)
+    public function indexAction($room_code)
     {
-        // TODO: Check that room code is valid first
+        // TODO: Check that room code is valid first?
 
         // Get QR Code from handler function using parameter
         $qr = $this->generateQR($room_code);
@@ -28,10 +30,16 @@ class QRController extends Controller
         return $response;
     }
 
-    public function generateQR($url) {
+    /**
+     * Pass in the room code, it will generate qr code for the full web URL to the room
+     *
+     * @param $room_code
+     * @return QrCode
+     */
+    public function generateQR($room_code) {
         // TODO: This should come somewhere in the config
-        $base_url = "https://localhost:8080/";
-        $full_url = $base_url . $url;
+        $base_url = "https://localhost:8080/"; // All urls should have this base
+        $full_url = $base_url . $room_code;
 
         $qrCode = new QrCode($full_url);
         $qrCode->setSize(300);
