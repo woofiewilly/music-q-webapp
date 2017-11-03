11711b9a768c345c6fb6282d3151b831f3228a83
abablani <abablani@users.noreply.github.com>
2017-11-03 11:12:02 -0400

    Adding spotify play integration

--- src/AppBundle/Controller/SpotifyCallbackController.php
+++ src/AppBundle/Controller/SpotifyCallbackController.php
@@ -28,8 +28,31 @@ class SpotifyCallbackController extends Controller
         $api->setAccessToken($accessToken);
         $me = $api->me();
         echo $me->display_name;
+        echo $api->play();
         // replace this example code with whatever you need
         return $this->render('spotify/spotifycallback.twig');
+    }
+    /**
+     * @Route("/play/", name="play")
+     */
+    public function playsong()
+    {
+        $session = new \SpotifyWebAPI\Session(
+            'f201b6c284a44bd6ac11f07430262360',
+            'a087e72614b344ca8919c6c79200335f',
+            'http://localhost:8080/callback'
+        );
+
+        // Request a access token using the code from Spotify
+        $session->requestAccessToken($_GET['code']);
+
+        $accessToken = $session->getAccessToken();
+        $refreshToken = $session->getRefreshToken();
+        $api = new \SpotifyWebAPI\SpotifyWebAPI();
+        $api->setAccessToken($accessToken);
+        $me = $api->me();
+        echo $me->display_name;
+        return new JsonResponse(array());
     }
 
 }
