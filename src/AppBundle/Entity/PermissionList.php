78103b699a5a6efd20a7e7c327eb0adf72183937
TristanPickett <tristan.pickett@gmail.com>
2017-11-27 20:40:48 -0500

    Added All Initial Settings For User Permissions Within A Room

--- src/AppBundle/Entity/PermissionList.php
+++ src/AppBundle/Entity/PermissionList.php
@@ -15,10 +15,155 @@ class PermissionList
      * @ORM\Column(name="playlistType", type="integer")
      */
     private $playlistType;
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="chatAdmin", type="bool")
+     */
+    private $chatAdmin;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="announcementsAdmin", type="bool")
+     */
+    private $announcementsAdmin;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="recommendationsAdmin", type="bool")
+     */
+    private $recommendationsAdmin;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="votingOnSongsAdmin", type="bool")
+     */
+    private $votingOnSongsAdmin;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="changeMusicAdmin", type="bool")
+     */
+    private $changeMusicAdmin;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="promoteUsersAdmin", type="bool")
+     */
+    private $promoteUsersAdmin;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="banUsersAdmin", type="bool")
+     */
+    private $banUsersAdmin;
+
+    /**
+     * @var bool
+     *
+     * If privilege level allows adding user to whitelist
+     * @ORM\Column(name="allowUserAdmin", type="bool")
+     */
+    private $allowUserAdmin;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="reportUserAdmin", type="bool")
+     */
+    private $reportUserAdmin;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="chatGuest", type="bool")
+     */
+    private $chatGuest;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="announcementsGuest", type="bool")
+     */
+    private $announcementsGuest;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="recommendationsGuest", type="bool")
+     */
+    private $recommendationsGuest;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="votingOnSongsGuest", type="bool")
+     */
+    private $votingOnSongsGuest;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="changeMusicGuest", type="bool")
+     */
+    private $changeMusicGuest;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="promoteUsersGuest", type="bool")
+     */
+    private $promoteUsersGuest;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="banUsersGuest", type="bool")
+     */
+    private $banUsersGuest;
+
+    /**
+     * @var bool
+     *
+     * If privilege level allows adding user to whitelist
+     * @ORM\Column(name="allowUserGuest", type="bool")
+     */
+    private $allowUserGuest;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="reportUserGuest", type="bool")
+     */
+    private $reportUserGuest;
 
     public function __construct()
     {
         $this->playlistType = 0;
+        $this->chatAdmin = TRUE;
+        $this->chatGuest = FALSE;
+        $this->announcementsAdmin = TRUE;
+        $this->announcementsGuest = FALSE;
+        $this->recommendationsAdmin = TRUE;
+        $this->recommendationsGuest = TRUE;
+        $this->votingOnSongsAdmin = TRUE;
+        $this->votingOnSongsGuest = TRUE;
+        $this->changeMusicAdmin = TRUE;
+        $this->changeMusicGuest = FALSE;
+        $this->promoteUsersAdmin = TRUE;
+        $this->promoteUsersGuest = FALSE;
+        $this->banUsersAdmin = TRUE;
+        $this->banUsersGuest = FALSE;
+        $this->allowUserAdmin = TRUE;
+        $this->allowUserGuest = FALSE;
+        $this->reportUserAdmin = TRUE;
+        $this->reportUserGuest = TRUE;
     }
 
     /**
@@ -35,4 +180,293 @@ class PermissionList
     {
         return $this->playlistType;
     }
+
+    /**
+     * @return bool
+     */
+    public function isChatAdmin()
+    {
+        return $this->chatAdmin;
+    }
+
+    /**
+     * @param bool $chatAdmin
+     */
+    public function setChatAdmin($chatAdmin)
+    {
+        $this->chatAdmin = $chatAdmin;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAnnouncementsAdmin()
+    {
+        return $this->announcementsAdmin;
+    }
+
+    /**
+     * @param bool $announcementsAdmin
+     */
+    public function setAnnouncementsAdmin($announcementsAdmin)
+    {
+        $this->announcementsAdmin = $announcementsAdmin;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRecommendationsAdmin()
+    {
+        return $this->recommendationsAdmin;
+    }
+
+    /**
+     * @param bool $recommendationsAdmin
+     */
+    public function setRecommendationsAdmin($recommendationsAdmin)
+    {
+        $this->recommendationsAdmin = $recommendationsAdmin;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isVotingOnSongsAdmin()
+    {
+        return $this->votingOnSongsAdmin;
+    }
+
+    /**
+     * @param bool $votingOnSongsAdmin
+     */
+    public function setVotingOnSongsAdmin($votingOnSongsAdmin)
+    {
+        $this->votingOnSongsAdmin = $votingOnSongsAdmin;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isChangeMusicAdmin()
+    {
+        return $this->changeMusicAdmin;
+    }
+
+    /**
+     * @param bool $changeMusicAdmin
+     */
+    public function setChangeMusicAdmin($changeMusicAdmin)
+    {
+        $this->changeMusicAdmin = $changeMusicAdmin;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isPromoteUsersAdmin()
+    {
+        return $this->promoteUsersAdmin;
+    }
+
+    /**
+     * @param bool $promoteUsersAdmin
+     */
+    public function setPromoteUsersAdmin($promoteUsersAdmin)
+    {
+        $this->promoteUsersAdmin = $promoteUsersAdmin;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isBanUsersAdmin()
+    {
+        return $this->banUsersAdmin;
+    }
+
+    /**
+     * @param bool $banUsersAdmin
+     */
+    public function setBanUsersAdmin($banUsersAdmin)
+    {
+        $this->banUsersAdmin = $banUsersAdmin;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAllowUserAdmin()
+    {
+        return $this->allowUserAdmin;
+    }
+
+    /**
+     * @param mixed $allowUserAdmin
+     */
+    public function setAllowUserAdmin($allowUserAdmin)
+    {
+        $this->allowUserAdmin = $allowUserAdmin;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isReportUserAdmin()
+    {
+        return $this->reportUserAdmin;
+    }
+
+    /**
+     * @param bool $reportUserAdmin
+     */
+    public function setReportUserAdmin($reportUserAdmin)
+    {
+        $this->reportUserAdmin = $reportUserAdmin;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isChatGuest()
+    {
+        return $this->chatGuest;
+    }
+
+    /**
+     * @param bool $chatGuest
+     */
+    public function setChatGuest($chatGuest)
+    {
+        $this->chatGuest = $chatGuest;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAnnouncementsGuest()
+    {
+        return $this->announcementsGuest;
+    }
+
+    /**
+     * @param bool $announcementsGuest
+     */
+    public function setAnnouncementsGuest($announcementsGuest)
+    {
+        $this->announcementsGuest = $announcementsGuest;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRecommendationsGuest()
+    {
+        return $this->recommendationsGuest;
+    }
+
+    /**
+     * @param bool $recommendationsGuest
+     */
+    public function setRecommendationsGuest($recommendationsGuest)
+    {
+        $this->recommendationsGuest = $recommendationsGuest;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isVotingOnSongsGuest()
+    {
+        return $this->votingOnSongsGuest;
+    }
+
+    /**
+     * @param bool $votingOnSongsGuest
+     */
+    public function setVotingOnSongsGuest($votingOnSongsGuest)
+    {
+        $this->votingOnSongsGuest = $votingOnSongsGuest;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isChangeMusicGuest()
+    {
+        return $this->changeMusicGuest;
+    }
+
+    /**
+     * @param bool $changeMusicGuest
+     */
+    public function setChangeMusicGuest($changeMusicGuest)
+    {
+        $this->changeMusicGuest = $changeMusicGuest;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isPromoteUsersGuest()
+    {
+        return $this->promoteUsersGuest;
+    }
+
+    /**
+     * @param bool $promoteUsersGuest
+     */
+    public function setPromoteUsersGuest($promoteUsersGuest)
+    {
+        $this->promoteUsersGuest = $promoteUsersGuest;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isBanUsersGuest()
+    {
+        return $this->banUsersGuest;
+    }
+
+    /**
+     * @param bool $banUsersGuest
+     */
+    public function setBanUsersGuest($banUsersGuest)
+    {
+        $this->banUsersGuest = $banUsersGuest;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAllowUserGuest()
+    {
+        return $this->allowUserGuest;
+    }
+
+    /**
+     * @param bool $allowUserGuest
+     */
+    public function setAllowUserGuest($allowUserGuest)
+    {
+        $this->allowUserGuest = $allowUserGuest;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isReportUserGuest()
+    {
+        return $this->reportUserGuest;
+    }
+
+    /**
+     * @param bool $reportUserGuest
+     */
+    public function setReportUserGuest($reportUserGuest)
+    {
+        $this->reportUserGuest = $reportUserGuest;
+    }
+
 }
