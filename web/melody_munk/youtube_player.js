4b32cc1f269995d1e11e1eea198b98c2f6821259
Brooks Van Buren <wvanbure@purdue.edu>
2017-12-01 12:46:17 -0500

    Small fix and improvement to test youtube

--- web/melody_munk/youtube_player.js
+++ web/melody_munk/youtube_player.js
@@ -14,8 +14,7 @@ function onYouTubeIframeAPIReady() {
         width: '640',
         videoId: '9W44NWYwa1g',
         events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
+            'onReady': onPlayerReady
         }
     });
 }
@@ -23,20 +22,6 @@ function onYouTubeIframeAPIReady() {
 // 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
     event.target.playVideo();
-}
-
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var done = false;
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
-    }
-}
-function stopVideo() {
-    player.stopVideo();
 }
 
 function loadPlaylistForm() {
